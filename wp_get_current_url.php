195668f62d21f7eb40b2f7598fd34d8c3376a548
Yeasir Arafat <arafat.dml@gmail.com>
2023-10-07 19:40:59 +0600

    Get current page full url

--- wp_get_current_url.php
+++ wp_get_current_url.php
@@ -25,3 +25,9 @@ if( strlen($wl_current_url) > 1 ){
 if ( in_array( $wl_current_url,  $wl_allowed_url) ) {
 	# Do Our Things Here
 }
+
+
+## Or Get the Full Page
+$page_url  = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+echo "Current URL: " . $page_url;
